e389d44d104acb0aff7564296c773a8bfef1c70a
Lazlo319 <Lazlogaming@gmail.com>
2019-10-07 20:41:11 +0200

    added boom prefab

---
+++ LD45/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensitivity;
+
+    void Update()
+    {
+        var x = new Vector2(Input.GetAxis("Left"), Input.GetAxis("Right"));
+        var y = new Vector2(Input.GetAxis("Up"), Input.GetAxis("Down"));
+
+        transform.Rotate(x* sensitivity, y* sensitivity, 0);
+    }
+}
