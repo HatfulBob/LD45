5e39cc4a0acc1aa1592f813bd8adce242c062d57
Lazlo319 <Lazlogaming@gmail.com>
2019-10-07 19:54:33 +0200

    added textures/color generation

---
+++ LD45/Assets/Scripts/Planet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Planet : MonoBehaviour
+{
+    public Material[] textures;
+    private MeshRenderer myMesh;
+    private Material texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMesh = GetComponent<MeshRenderer>();
+
+        //pick random texture
+        texture = textures[Random.Range(0, textures.Length)];
+
+        //apply texture
+        myMesh.material = texture;
+        myMesh.material.color = RandomColor();
+    }
+
+    private Color RandomColor()
+    {
+        return Color.HSVToRGB(Random.Range(0, .9f), 1, Random.Range(0.6f, 1));
+    }
+}
